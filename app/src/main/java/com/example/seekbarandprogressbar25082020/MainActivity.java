d740710d871283196c92529072f7744b377c74ba
phatdroid94 <phatdroid94@gmail.com>
2020-10-06 20:07:03 +0700

    gan background color

--- app/src/main/java/com/example/seekbarandprogressbar25082020/MainActivity.java
+++ app/src/main/java/com/example/seekbarandprogressbar25082020/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.seekbarandprogressbar25082020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     TextView mTvRGB,mTvHex;
     SeekBar mSkR,mSkA,mSkB,mSkG;
     LinearLayout mBackground;
+    int mRed,mGreen,mBlue,mAlpha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +26,46 @@ public class MainActivity extends AppCompatActivity {
         mSkR = findViewById(R.id.seekbarRed);
         mBackground = findViewById(R.id.linearBackGround);
 
+        mRed = mGreen = mBlue = mAlpha = 0;
+
+        mSkA.setOnSeekBarChangeListener(onSeekBarChangeListener);
+        mSkR.setOnSeekBarChangeListener(onSeekBarChangeListener);
+        mSkG.setOnSeekBarChangeListener(onSeekBarChangeListener);
+        mSkB.setOnSeekBarChangeListener(onSeekBarChangeListener);
+
         // Task 1 : thực hiện kéo seekbar và gắn màu cho background
         // Task 2 : Gắn giá trị màu A _ R _ G _ B
-        // Task 3 : dịch mã màu theo hệ hexa
-
+        // Task 3 : dịch mã màu theo hệ hex
     }
+    private SeekBar.OnSeekBarChangeListener onSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            switch (seekBar.getId()){
+                case R.id.seekbarAlpha :
+                    mAlpha = progress;
+                    break;
+                case R.id.seekbarRed :
+                    mRed = progress;
+                    break;
+                case R.id.seekbarGreen :
+                    mGreen = progress;
+                    break;
+                case R.id.seekbarBlue :
+                    mBlue = progress;
+                    break;
+            }
+            mBackground.setBackgroundColor(Color.argb(mAlpha,mRed,mGreen,mBlue));
+            mTvRGB.setText(String.format("RBG %d %d %d %d",mAlpha,mRed,mGreen,mBlue));
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
 }
